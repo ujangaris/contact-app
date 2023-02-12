e89de0228cf56633c02d4188d30efbdfddb6cf03
ujangaja <ujangaja@gmail.com>
2023-02-13 06:16:07 +0700

    Desain aplikasi dengan bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import List from "./List"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="px-3 py-3">My Contact List</h1>
+
+      <form className="px-3 py-4">
+        <div className="form-group">
+          <label htmlFor="">Name</label>
+          <input type="text" className="form-control" name="name" />
+        </div>
+        <div className="form-group mt-3">
+          <label htmlFor="">No. Telp</label>
+          <input type="text" className="form-control" name="telp" />
+        </div>
+        <div>
+          <button type="submit" className="btn btn-primary w-100 mt-3">
+            Save
+          </button>
+        </div>
+      </form>
+
+      <List />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
