53276f5c32c4c37aca71882fb0a995c47174705a
ujangaja <ujangaja@gmail.com>
2023-02-13 06:26:47 +0700

    State untuk menampilkan dan mengoper data dengan props

--- README.md
+++ README.md
@@ -8,3 +8,13 @@
         2.  pasang react-bootstrap pada App.js
             import "bootstrap/dist/css/bootstrap.min.css"
         3.  App.css
+
+## State untuk menampilkan dan mengoper data dengan props
+
+    Todo :
+        1.  App.js
+            - buat state untuk menyimpan data contact
+            - oper data props kedalam list
+        2.  List.js
+            - didalam list akan menerima data props yang berupa data
+            - menampilkan data dengan map
--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import List from "./List"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
+import { useState } from "react"
 
 function App() {
+  // buat state untuk menyimpan data contact
+  const [contacts, setContacts] = useState([
+    {
+      id: 1,
+      name: "John Doe",
+      telp: "08815646413",
+    },
+    {
+      id: 2,
+      name: "Alex Pick",
+      telp: "08815646413",
+    },
+  ])
   return (
     <div className="App">
       <h1 className="px-3 py-3">My Contact List</h1>
@@ -22,8 +36,8 @@ function App() {
           </button>
         </div>
       </form>
-
-      <List />
+      {/* oper data props kedalam list  */}
+      <List data={contacts} />
     </div>
   )
 }
